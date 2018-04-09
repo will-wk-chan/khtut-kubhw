ac407dcc7dc67daf4ddf49809ee874b472c0a342
willc <kangar_o@hotmail.com>
2018-04-09 13:29:00 -0700

    commit init bootstrap/kube-gcp-workers.sh

---
+++ bootstrap/kube-gcp-workers.sh
@@ -0,0 +1,205 @@
+#!/bin/bash
+
+# README
+# Provisioning a Kubernetes Worker Node
+# https://github.com/kelseyhightower/kubernetes-the-hard-way/blob/master/docs/09-bootstrapping-kubernetes-workers.md
+# EDIT (below)
+GS_BUCKET=dev-bucket-ks
+# EDIT (above)
+
+# copy in ca cert
+gsutil cp gs://${GS_BUCKET}/ca.pem /tmp/
+gsutil cp gs://${GS_BUCKET}/kube-proxy.kubeconfig /tmp/
+
+# install cfssl to generate certs later on
+wget -q --show-progress --https-only --timestamping \
+  https://pkg.cfssl.org/R1.2/cfssl_linux-amd64 \
+  https://pkg.cfssl.org/R1.2/cfssljson_linux-amd64
+
+chmod +x cfssl_linux-amd64 cfssljson_linux-amd64
+sudo mv cfssl_linux-amd64 /usr/local/bin/cfssl
+sudo mv cfssljson_linux-amd64 /usr/local/bin/cfssljson
+
+# Download and Install Worker Binaries
+sudo apt-get -y install socat
+
+wget -q --show-progress --https-only --timestamping \
+  https://github.com/containernetworking/plugins/releases/download/v0.6.0/cni-plugins-amd64-v0.6.0.tgz \
+  https://github.com/containerd/cri-containerd/releases/download/v1.0.0-beta.1/cri-containerd-1.0.0-beta.1.linux-amd64.tar.gz \
+  https://storage.googleapis.com/kubernetes-release/release/v1.9.0/bin/linux/amd64/kubectl \
+  https://storage.googleapis.com/kubernetes-release/release/v1.9.0/bin/linux/amd64/kube-proxy \
+  https://storage.googleapis.com/kubernetes-release/release/v1.9.0/bin/linux/amd64/kubelet
+
+sudo mkdir -p \
+  /etc/cni/net.d \
+  /opt/cni/bin \
+  /var/lib/kubelet \
+  /var/lib/kube-proxy \
+  /var/lib/kubernetes \
+  /var/run/kubernetes
+
+sudo tar -xvf cni-plugins-amd64-v0.6.0.tgz -C /opt/cni/bin/
+sudo tar -xvf cri-containerd-1.0.0-beta.1.linux-amd64.tar.gz -C /
+chmod +x kubectl kube-proxy kubelet
+sudo mv kubectl kube-proxy kubelet /usr/local/bin/
+
+# Configure CNI Networking
+# Retrieve the Pod CIDR range for the current compute instance:
+POD_CIDR=$(curl -s -H "Metadata-Flavor: Google" \
+  http://metadata.google.internal/computeMetadata/v1/instance/attributes/pod-cidr)
+
+# Create the bridge network configuration file:
+cat > 10-bridge.conf <<EOF
+{
+    "cniVersion": "0.3.1",
+    "name": "bridge",
+    "type": "bridge",
+    "bridge": "cnio0",
+    "isGateway": true,
+    "ipMasq": true,
+    "ipam": {
+        "type": "host-local",
+        "ranges": [
+          [{"subnet": "${POD_CIDR}"}]
+        ],
+        "routes": [{"dst": "0.0.0.0/0"}]
+    }
+}
+EOF
+
+# Create the loopback network configuration file:
+cat > 99-loopback.conf <<EOF
+{
+    "cniVersion": "0.3.1",
+    "type": "loopback"
+}
+EOF
+
+# Move the network configuration files to the CNI configuration directory:
+sudo mv 10-bridge.conf 99-loopback.conf /etc/cni/net.d/
+
+# Configure the Kubelet
+#   requires certs
+cat > ${HOSTNAME}-csr.json <<EOF
+{
+  "CN": "system:node:${HOSTNAME}",
+  "key": {
+    "algo": "rsa",
+    "size": 2048
+  },
+  "names": [
+    {
+      "C": "US",
+      "L": "Portland",
+      "O": "system:nodes",
+      "OU": "Kubernetes The Hard Way",
+      "ST": "Oregon"
+    }
+  ]
+}
+EOF
+
+EXTERNAL_IP=$(gcloud compute instances describe ${HOSTNAME} \
+  --format 'value(networkInterfaces[0].accessConfigs[0].natIP)')
+
+INTERNAL_IP=$(gcloud compute instances describe ${HOSTNAME} \
+  --format 'value(networkInterfaces[0].networkIP)')
+
+# generate node certs
+cfssl gencert \
+  -ca=ca.pem \
+  -ca-key=ca-key.pem \
+  -config=ca-config.json \
+  -hostname=${HOSTNAME},${EXTERNAL_IP},${INTERNAL_IP} \
+  -profile=kubernetes \
+  ${HOSTNAME}-csr.json | cfssljson -bare ${HOSTNAME}
+
+# Generate kubeconfig file
+KUBERNETES_PUBLIC_ADDRESS=$(gcloud compute addresses describe kubernetes-the-hard-way \
+  --region $(gcloud config get-value compute/region) \
+  --format 'value(address)')
+kubectl config set-cluster kubernetes-the-hard-way \
+    --certificate-authority=ca.pem \
+    --embed-certs=true \
+    --server=https://${KUBERNETES_PUBLIC_ADDRESS}:6443 \
+    --kubeconfig=${HOSTNAME}.kubeconfig
+
+kubectl config set-credentials system:node:${HOSTNAME} \
+    --client-certificate=${HOSTNAME}.pem \
+    --client-key=${HOSTNAME}-key.pem \
+    --embed-certs=true \
+    --kubeconfig=${HOSTNAME}.kubeconfig
+
+kubectl config set-context default \
+    --cluster=kubernetes-the-hard-way \
+    --user=system:node:${HOSTNAME} \
+    --kubeconfig=${HOSTNAME}.kubeconfig
+
+kubectl config use-context default --kubeconfig=${HOSTNAME}.kubeconfig
+
+sudo mv ${HOSTNAME}-key.pem ${HOSTNAME}.pem /var/lib/kubelet/
+sudo mv ${HOSTNAME}.kubeconfig /var/lib/kubelet/kubeconfig
+sudo mv /tmp/ca.pem /var/lib/kubernetes/
+
+# Create the kubelet.service systemd unit file:
+
+cat > kubelet.service <<EOF
+[Unit]
+Description=Kubernetes Kubelet
+Documentation=https://github.com/kubernetes/kubernetes
+After=cri-containerd.service
+Requires=cri-containerd.service
+
+[Service]
+ExecStart=/usr/local/bin/kubelet \\
+  --allow-privileged=true \\
+  --anonymous-auth=false \\
+  --authorization-mode=Webhook \\
+  --client-ca-file=/var/lib/kubernetes/ca.pem \\
+  --cloud-provider= \\
+  --cluster-dns=10.32.0.10 \\
+  --cluster-domain=cluster.local \\
+  --container-runtime=remote \\
+  --container-runtime-endpoint=unix:///var/run/cri-containerd.sock \\
+  --image-pull-progress-deadline=2m \\
+  --kubeconfig=/var/lib/kubelet/kubeconfig \\
+  --network-plugin=cni \\
+  --pod-cidr=${POD_CIDR} \\
+  --register-node=true \\
+  --runtime-request-timeout=15m \\
+  --tls-cert-file=/var/lib/kubelet/${HOSTNAME}.pem \\
+  --tls-private-key-file=/var/lib/kubelet/${HOSTNAME}-key.pem \\
+  --v=2
+Restart=on-failure
+RestartSec=5
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Configure the Kubernetes Proxy
+sudo mv /tmp/kube-proxy.kubeconfig /var/lib/kube-proxy/kubeconfig
+
+cat > kube-proxy.service <<EOF
+[Unit]
+Description=Kubernetes Kube Proxy
+Documentation=https://github.com/kubernetes/kubernetes
+
+[Service]
+ExecStart=/usr/local/bin/kube-proxy \\
+  --cluster-cidr=10.200.0.0/16 \\
+  --kubeconfig=/var/lib/kube-proxy/kubeconfig \\
+  --proxy-mode=iptables \\
+  --v=2
+Restart=on-failure
+RestartSec=5
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# start worker' service
+sudo mv kubelet.service kube-proxy.service /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl enable containerd cri-containerd kubelet kube-proxy
+sudo systemctl start containerd cri-containerd kubelet kube-proxy
